4e95e0f8944fcfeedb9ae699c3933832085191a5
Dmitry Komissarov <komdimkom@gmail.com>
2023-07-08 18:47:19 +0300

    add titleLabel and ImageLabel

---
+++ TotalBill/MainViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  TotalBill
+//
+//  Created by mac on 08.07.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Total Bill"
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next Bold", size: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+
+    }
+
+    
+    func setupViews() {
+        view.backgroundColor = #colorLiteral(red: 0.9813271165, green: 0.9813271165, blue: 0.9813271165, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        view.addSubview(logoImageView)
+    }
+
+
+}
+
+extension MainViewController {
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            logoImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)
+        ])
+    }
+}
+
